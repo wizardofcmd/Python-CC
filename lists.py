4a93a64a7818772e7f48c6c29e20bcc72e7a81f0
Sorin Nechifor <x18393913@student.ncirl.ie>
2020-09-06 16:54:49 +0100

    List insertion and removal

---
+++ lists.py
@@ -0,0 +1,28 @@
+guests = ['Laura Les', 'Dorian Electra', 'Catherine Slater']
+for x in range(0, 3):
+    print(guests[x] + " would you like to attend my dinner party?")
+
+print("\n" + guests[2] + " won't be able to make it.\nWe will be inviting Ayesha Erotica instead.\n")
+del guests[2]
+guests.append("Ayesha Erotica")
+for x in range(0, 3):
+    print(guests[x] + " would you like to attend my dinner party?")
+
+print("\nWell look at that! We found a bigger dinner table!\n")
+guests.insert(0, "That Kid")
+guests.insert(2, "Osquinn")
+guests.append("Hisoka Morrow")
+
+for x in range(len(guests)):
+    print(guests[x] + " would you like to attend my dinner party?")
+
+print("\nOopsie daisies. The table won't make it in time. Unfortunately, we only have room for two people!\n")
+for x in range(5, 1, -1):
+    popped = guests.pop(x)
+    print("Sorry " + popped + ", we don't have space at the table for you.")
+print()
+
+for x in range(1, -1, -1):
+    print(guests[x] + " you guys can still attend the dinner party.")
+    del guests[x]
+print(guests)
---
+++ name_cases.py
@@ -0,0 +1,15 @@
+name_one = "Linda"
+print("Hello " + name_one + " would you like to learn some Python today?")
+
+name_two = "Derek Hack"
+print(name_two.upper() + "\n" + name_two.lower() + "\n" + name_two.title())
+
+quote = "A person who never made a mistake never tried anything new."
+print("Some dude once said: " + quote)
+
+famous_man = "Tom Hardy"
+message = " once said: A person who never made a mistake never tired anyting new."
+print(famous_man + message)
+
+spaces = " 100 gecs "
+print(spaces.rstrip() + "\n" + spaces.lstrip() + "\n" + spaces.strip())
