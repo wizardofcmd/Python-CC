5f117c5fb11c7f8e6a21c0eb8466977a15187542
Sorin Nechifor <sorinnechiforora@gmail.com>
2020-09-12 22:50:41 +0100

    Chapter 9 - Classes

---
+++ firstclass.py
@@ -0,0 +1,10 @@
+class Dog():
+    def __init___(self, name, age):
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        print(self.name.titleI() + ' is now sitting.')
+
+    def roll_over(self):
+        print(self.name.title() + ' rolled over!')
