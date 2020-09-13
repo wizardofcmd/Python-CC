efb2295c28da81f178c01ec347e9e9461e973452
Sorin Nechifor <sorinnechiforora@gmail.com>
2020-09-13 20:34:50 +0100

    Chapter 9 - Classes

--- firstclass.py
+++ firstclass.py
@@ -1,10 +1,17 @@
-class Dog():
-    def __init___(self, name, age):
-        self.name = name
-        self.age = age
+class Restaurant():
+    """Class representing a restaurant"""
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
 
-    def sit(self):
-        print(self.name.titleI() + ' is now sitting.')
+    def describe_restaurant(self):
+        print('The name of the restaurant is ' + self.restaurant_name + '.')
+        print('Cuisine: ' + self.cuisine_type + '.')
 
-    def roll_over(self):
-        print(self.name.title() + ' rolled over!')
+    def open_restaurant(self):
+        print('\nThe restaurant is now open!')
+
+restaurant = Restaurant('Luigis', 'Italian')
+print(restaurant.restaurant_name + " " + restaurant.cuisine_type)
+restaurant.describe_restaurant()
+restaurant.open_restaurant()
