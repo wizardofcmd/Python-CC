d4993bc3cf028a798cf13e794bec64c51341205a
Sorin Nechifor <sorinnechiforora@gmail.com>
2020-09-13 21:56:56 +0100

    Chapter 9 - Classes

--- firstclass.py
+++ firstclass.py
@@ -3,6 +3,7 @@ class Restaurant():
     def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_served = 0
 
     def describe_restaurant(self):
         print('The name of the restaurant is ' + self.restaurant_name + '.')
@@ -11,10 +12,25 @@ class Restaurant():
     def open_restaurant(self):
         print('\nThe restaurant is now open!')
 
+    def set_number_served(self, number_served):
+        self.number_served = number_served
+
+    def increment_number_served(self, number_served):
+        if number_served > 0:
+            self.number_served += number_served
+        else:
+            print("Can't serve minus people!")
+
 restaurant = Restaurant('Luigis', 'Italian')
-print(restaurant.restaurant_name + " " + restaurant.cuisine_type)
+print(restaurant.restaurant_name + " " + restaurant.cuisine_type + \
+" " + str(restaurant.number_served) + ".")
 restaurant.describe_restaurant()
 restaurant.open_restaurant()
+
+restaurant.set_number_served(2)
+print(str(restaurant.number_served))
+restaurant.increment_number_served(4)
+print(str(restaurant.number_served) + "\n")
 
 class User():
     """Class representing a user profile with many attributes to describe them."""
@@ -24,6 +40,7 @@ class User():
         self.age = age
         self.height = height
         self.eye_colour = eye_colour
+        self.login_attempts = 0
 
     def describe_user(self):
         print(self.first_name + " " + self.last_name + " " + str(self.age) + " "\
@@ -33,8 +50,20 @@ class User():
         print(self.__dict__)
 
     def greet_user(self):
-        print('Hello ' + self.first_name + ', welcome.')
+        print('Hello ' + self.first_name + ', welcome.\n')
+
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
 
 user = User('Kai', 'Havertz', 21, '189cm', 'Hazel')
 user.describe_user()
 user.greet_user()
+
+print(user.login_attempts)
+user.increment_login_attempts()
+print(user.login_attempts)
+user.reset_login_attempts()
+print(user.login_attempts)
