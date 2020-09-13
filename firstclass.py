f92103f4d5bffa917417e10e848d1fe18d9f861f
Sorin Nechifor <sorinnechiforora@gmail.com>
2020-09-13 21:19:40 +0100

    Chapter 9 - Classes

--- firstclass.py
+++ firstclass.py
@@ -15,3 +15,26 @@ restaurant = Restaurant('Luigis', 'Italian')
 print(restaurant.restaurant_name + " " + restaurant.cuisine_type)
 restaurant.describe_restaurant()
 restaurant.open_restaurant()
+
+class User():
+    """Class representing a user profile with many attributes to describe them."""
+    def __init__(self, first_name, last_name, age, height, eye_colour):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.height = height
+        self.eye_colour = eye_colour
+
+    def describe_user(self):
+        print(self.first_name + " " + self.last_name + " " + str(self.age) + " "\
+        + self.height + " " + self.eye_colour + ".")
+        # OR use __dict__, which prints the attributes and valuesof the class
+        # in a dictionary format.
+        print(self.__dict__)
+
+    def greet_user(self):
+        print('Hello ' + self.first_name + ', welcome.')
+
+user = User('Kai', 'Havertz', 21, '189cm', 'Hazel')
+user.describe_user()
+user.greet_user()
